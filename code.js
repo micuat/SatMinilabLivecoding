929d15c185aa8db73f2ee074b31a55f0c3ef0f92
naoto <micuat@gmail.com>
2017-02-15 16:38:37 -0500

    a

--- code.js
+++ code.js
@@ -1,9 +1,19 @@
 background(0);
 
 translate(width/2, height/2);
-rotateY(frameCount / 100);
-stroke(255);
-noFill();
-box(height / 2);
-rotateZ(frameCount / 100);
-box(height / 2 / 1.4);
+
+var i;
+for(i = 0; i < 16; i++) {
+  switch(i % 3) {
+    case 0:
+    rotateZ(frameCount / 200);
+    case 1:
+    rotateY(-frameCount / 100);
+    case 2:
+    rotateX(frameCount / 300);
+  }
+  stroke(255);
+  noFill();
+  box(height / 2);
+  scale(0.95, 0.95, 0.95);
+}
