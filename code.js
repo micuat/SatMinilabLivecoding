9e726832ff42a54f36cf0375d20691ecde70cada
naoto <micuat@gmail.com>
2017-02-15 16:42:49 -0500

    a

--- code.js
+++ code.js
@@ -2,18 +2,22 @@ background(0);
 
 translate(width/2, height/2);
 
-var i;
-for(i = 0; i < 16; i++) {
-  switch(i % 3) {
-    case 0:
-    rotateZ(frameCount / 200);
-    case 1:
-    rotateY(-frameCount / 100);
-    case 2:
-    rotateX(frameCount / 300);
+var i, j;
+for(i = -16; i < 16; i++) {
+  for(j = -16; j < 16; j++) {
+    pushMatrix();
+    translate(j * 50, i * 50, 0);
+    switch((i+16) % 3) {
+      case 0:
+      rotateZ(frameCount / 200);
+      case 1:
+      rotateY(-frameCount / 100);
+      case 2:
+      rotateX(frameCount / 300);
+    }
+    stroke(255);
+    noFill();
+    box(50);
+    popMatrix();
   }
-  stroke(255);
-  noFill();
-  box(height / 2);
-  scale(0.95, 0.95, 0.95);
 }
