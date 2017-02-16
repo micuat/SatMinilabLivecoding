e9e262fdb3b1814e01140d15d714964e80608f7f
naoto <micuat@gmail.com>
2017-02-16 17:52:35 -0500

    a

--- code.js
+++ code.js
@@ -2,6 +2,10 @@ background(0);
 
 function Agent () {
     this.size = 10;
+    this.x = 0;
+    this.y = 0;
+    this.j = 0;
+    this.i = 0;
 }
 
 define("agents", new java.util.HashMap());
@@ -16,6 +20,10 @@ if(doInitialize > 0) {
     for(j = -16; j < 16; j++) {
       agents.put(i + "," + j, new Agent);
       agents[i + "," + j].size = 17 + i;
+      agents[i + "," + j].j = j;
+      agents[i + "," + j].i = i;
+      agents[i + "," + j].x = j * 50;
+      agents[i + "," + j].y = i * 50;
     }
   }
 
@@ -27,22 +35,19 @@ translate(width/2, height/2);
 
 pointLight(255, 255, 255, 0, 0, 100);
 
-var i, j;
-for(i = -16; i < 16; i++) {
-  for(j = -16; j < 16; j++) {
-    pushMatrix();
-    translate(j * 50, i * 50, 0);
-    switch((i+16) % 3) {
-      case 0:
-      rotateZ(frameCount / 200);
-      case 1:
-      rotateY(-frameCount / 100);
-      case 2:
-      rotateX(frameCount / 300);
-    }
-    stroke(255);
-    fill(255);
-    box(agents[i + "," + j].size);
-    popMatrix();
+for each(var agent in agents.values()) {
+  pushMatrix();
+  translate(agent.x, agent.y, 0);
+  switch((agent.i+16) % 3) {
+    case 0:
+    rotateZ(frameCount / 200);
+    case 1:
+    rotateY(-frameCount / 100);
+    case 2:
+    rotateX(frameCount / 300);
   }
+  stroke(255);
+  fill(255);
+  box(agent.size);
+  popMatrix();
 }
